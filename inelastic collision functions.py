7d49fd2d89e0aa4eae0e8c2f52f8a23a93a4056a
ejfminns <ejfminns@gmail.com>
2025-12-03 11:40:12 +0100

    Added forgotten else statement
    y is the same for v and w so its set up is the same in both functions, I was able to just copy the else statement from the function vAfter

--- inelastic collision functions.py
+++ inelastic collision functions.py
@@ -20,6 +20,9 @@ def wAfter(v, w, r, h, m , theta, gamma, z_star, theta_c):
     if (theta >= 0 and theta < np.pi/2) or (theta > -np.pi and theta < -np.pi/2):
         y = z_star * np.sin(abs(theta) - theta_c)
 
+    else:
+        y = -z_star * np.sin(abs(theta) - theta_c)
+
     I = (1/12) * m * (3*r**2 + h**2)
     
     w_change = -(1 + gamma) * (m*y / (I + m*y**2)) * (v + y*w)
@@ -31,6 +34,7 @@ def wAfter(v, w, r, h, m , theta, gamma, z_star, theta_c):
 # sample:
 
 vv = vAfter(0.5, 0.5, 12e-3, 2e-3, 7e-3, np.pi/3, 0.9, np.sqrt((12e-3)**2 + (1e-3)**2), np.arctan(2e-3 / (2*12e-3)))
+
 
 
 
