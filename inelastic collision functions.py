0ede6047d997e3c8f379b05c7c54e8818be5bca1
ejfminns <ejfminns@gmail.com>
2025-12-09 17:13:32 +0100

    Capitalized constants
    Also, commented out code for w since it is constant

--- inelastic collision functions.py
+++ inelastic collision functions.py
@@ -1,19 +1,21 @@
 import numpy as np
 
-def vAfter(v, w, r, h, m , theta, gamma, z_star, theta_c):
+def vAfter(v, W, R, H, M , theta, GAMMA, Z_STAR, THETA_C):
     
     if (theta >= 0 and theta < np.pi/2) or (theta > -np.pi and theta < -np.pi/2):
-        y = z_star * np.sin(abs(theta) - theta_c)
+        y = Z_STAR * np.sin(abs(theta) - THETA_C)
         
     else:
-        y = -z_star * np.sin(abs(theta) - theta_c)
+        y = -Z_STAR * np.sin(abs(theta) - THETA_C)
     
-    I = (1/12) * m * (3*r**2 + h**2)
+    I = (1/12) * M * (3*R**2 + H**2)
     
-    v_change = -(1 + gamma) * (I / (I + m*y**2)) * (v + y*w)
+    v_change = -(1 + GAMMA) * (I / (I + M*y**2)) * (v + y*W)
     
     v += v_change
     return v
+'''
+# W constant
 
 def wAfter(v, w, r, h, m , theta, gamma, z_star, theta_c):
 
@@ -29,11 +31,12 @@ def wAfter(v, w, r, h, m , theta, gamma, z_star, theta_c):
     
     w += w_change
     return w
-
+'''
 
 # sample:
 
 vv = vAfter(0.5, 0.5, 12e-3, 2e-3, 7e-3, np.pi/3, 0.9, np.sqrt((12e-3)**2 + (1e-3)**2), np.arctan(2e-3 / (2*12e-3)))
+
 
 
 
