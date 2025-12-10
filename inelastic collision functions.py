3f68cef59e3221a87737bf9a042e80ea4d8109de
ejfminns <ejfminns@gmail.com>
2025-12-10 11:07:11 +0100

    Fixed mistakes

--- inelastic collision functions.py
+++ inelastic collision functions.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def vAfter(v, W, R, H, M , theta, GAMMA, Z_STAR, THETA_C):
+def vAfter(v, w, R, H, M , theta, GAMMA, Z_STAR, THETA_C):
     
     if (theta >= 0 and theta < np.pi/2) or (theta > -np.pi and theta < -np.pi/2):
         y = Z_STAR * np.sin(abs(theta) - THETA_C)
@@ -10,12 +10,10 @@ def vAfter(v, W, R, H, M , theta, GAMMA, Z_STAR, THETA_C):
     
     I = (1/12) * M * (3*R**2 + H**2)
     
-    v_change = -(1 + GAMMA) * (I / (I + M*y**2)) * (v + y*W)
+    v_change = -(1 + GAMMA) * (I / (I + M*y**2)) * (v + y*w)
     
     v += v_change
     return v
-'''
-# W constant
 
 def wAfter(v, w, r, h, m , theta, gamma, z_star, theta_c):
 
@@ -31,11 +29,11 @@ def wAfter(v, w, r, h, m , theta, gamma, z_star, theta_c):
     
     w += w_change
     return w
-'''
 
 # sample:
 
 vv = vAfter(0.5, 0.5, 12e-3, 2e-3, 7e-3, np.pi/3, 0.9, np.sqrt((12e-3)**2 + (1e-3)**2), np.arctan(2e-3 / (2*12e-3)))
+
 
 
 
